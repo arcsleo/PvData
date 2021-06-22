6f7471dde8607a4629bd031173f40acfb9147037
arcsleo <archishnair95@gmail.com>
2021-06-23 02:24:18 +0530

    first commit

--- src/components/FirstTab/index.tsx
+++ src/components/FirstTab/index.tsx
@@ -4,14 +4,14 @@ import PDF from '../../assets/image/Pdf.png';
 
 export default function FirstTab(props: any){
 
-    const [tabData, setTabData] = React.useState([
+    const tabData = [
         { id: 1, Color: 'rgba(0, 135, 83, 0.8)', Title: 'Country Solar Outlook', Image: PDF , Head: 'Understand your market', Description: 'Get live updates on  the solar  power and utility market across more than 94 countries globally. Find out how country policies and regulation affect your solar energy project.' },
         { id: 2, Color: 'rgba(0, 135, 83, 0.68)', Title: 'Sensitivity Analysis', Image: PDF, Head: 'Account for future uncertainty', Description: 'Run a sensitivity analysis on your solar energy project to vary the assumptions of your financial outputs under different scenarios.' },
         { id: 3, Color: 'rgba(0, 135, 83, 0.56)', Title: 'Environmental Report', Image: PDF , Head: 'Know your environmental and social risk', Description: 'Identify and mitigate major environmental, social and governnance risks (ESG) that your solar energy project may be exposed to during its lifecycle' },
         { id: 4, Color: 'rgba(0, 135, 83, 0.44)', Title: 'Technical & Design', Image: PDF , Head: 'Conduct your technical analysis', Description: 'Measure the electricity yield of your solar PV system; run a forecast analysis on your project sites’ irradiance, temperature and electricity generation.' },
         { id: 5, Color: 'rgba(0, 135, 83, 0.32)', Title: 'Financial Summary', Image: PDF , Head: 'Get your Project finance overview', Description: 'Get an overview of key financial metrics of your solar energy project to better assess financial viability.' },
         { id: 6, Color: 'rgba(0, 135, 83, 0.2)', Title: 'Balance Sheet', Image: PDF , Head: 'Get your Cashflow overview', Description: 'View your solar energy projects financial statement, detailing the balance of the revenue and expenditure over your projects lifecycle.'}
-    ]);
+    ];
 
     const [activeTab, setactiveTab] = React.useState(1);
 
